94720c047cf5f47ad9aaaf6c084216b3dc921b38
RobertZagorski <robertzagorsky@gmail.com>
2016-12-08 15:28:48 +0100

    Dependency injection - integrated Dagger2 search component into search activity.

---
+++ app/src/main/java/com/rzagorski/thingithubclient/utils/interfaces/ComponentCreator.java
@@ -0,0 +1,10 @@
+package com.rzagorski.thingithubclient.utils.interfaces;
+
+/**
+ * Created by Robert Zagórski on 2016-12-08.
+ */
+
+public interface ComponentCreator<T> {
+
+    T getComponent();
+}
--- app/src/main/java/com/rzagorski/thingithubclient/view/search/SearchActivity.java
+++ app/src/main/java/com/rzagorski/thingithubclient/view/search/SearchActivity.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.rzagorski.thingithubclient.R;
+import com.rzagorski.thingithubclient.ThinGithubClientApplication;
+import com.rzagorski.thingithubclient.di.search.SearchActivityComponent;
+import com.rzagorski.thingithubclient.di.search.SearchActivityModule;
+import com.rzagorski.thingithubclient.utils.interfaces.ComponentCreator;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements ComponentCreator<SearchActivityComponent> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        getComponent().inject(this);
+    }
+
+    @Override
+    public SearchActivityComponent getComponent() {
+        return ((ThinGithubClientApplication) getApplicationContext())
+                .getApplicationComponent()
+                .provide(new SearchActivityModule(this));
     }
 }
